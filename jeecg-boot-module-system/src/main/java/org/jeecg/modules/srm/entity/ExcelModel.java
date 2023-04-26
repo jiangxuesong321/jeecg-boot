c2e904a36a7b4f64de493f3837f2913b9bd3b0bc
jiangxuesong321 <1393143765@qq.com>
2023-04-26 09:27:42 +0800

    导入物料数据 atom

---
+++ jeecg-boot-module-system/src/main/java/org/jeecg/modules/srm/entity/ExcelModel.java
@@ -0,0 +1,84 @@
+package org.jeecg.modules.srm.entity;
+
+public class ExcelModel {
+
+    private String cell00;
+
+    private String cell01;
+
+    private String cell02;
+
+    private String cell03;
+
+    private String cell04;
+
+    private String cell05;
+
+    private String cell06;
+
+    private String cell07;
+
+    public String getCell00() {
+        return cell00;
+    }
+
+    public void setCell00(String cell00) {
+        this.cell00 = cell00;
+    }
+
+    public String getCell01() {
+        return cell01;
+    }
+
+    public void setCell01(String cell01) {
+        this.cell01 = cell01;
+    }
+
+    public String getCell02() {
+        return cell02;
+    }
+
+    public void setCell02(String cell02) {
+        this.cell02 = cell02;
+    }
+
+    public String getCell03() {
+        return cell03;
+    }
+
+    public void setCell03(String cell03) {
+        this.cell03 = cell03;
+    }
+
+    public String getCell04() {
+        return cell04;
+    }
+
+    public void setCell04(String cell04) {
+        this.cell04 = cell04;
+    }
+
+    public String getCell05() {
+        return cell05;
+    }
+
+    public void setCell05(String cell05) {
+        this.cell05 = cell05;
+    }
+
+    public String getCell06() {
+        return cell06;
+    }
+
+    public void setCell06(String cell06) {
+        this.cell06 = cell06;
+    }
+
+    public String getCell07() {
+        return cell07;
+    }
+
+    public void setCell07(String cell07) {
+        this.cell07 = cell07;
+    }
+}
